6123e94ef1bfc60a9283cd3917f68b9dccb096f1
Harry Li <reaperxxxiiii@gmail.com>
2022-06-26 14:43:53 +1200

    add item to list

--- CSC2 Julies Party Hire.py
+++ CSC2 Julies Party Hire.py
@@ -24,3 +24,16 @@ def print_camp_details():
     Label(main_window, font=("Helvetica 10 bold"),
           text="Amount of items").grid(column=3, row=7)
 
+
+# add each item in the list into its own row
+    while name_count < total_entries:
+        Label(main_window, text=name_count).grid(column=0, row=name_count+8)
+        Label(main_window, text=(hire_details[name_count][0])).grid(
+            column=1, row=name_count+8)
+        Label(main_window, text=(hire_details[name_count][1])).grid(
+            column=2, row=name_count+8)
+        Label(main_window, text=(hire_details[name_count][2])).grid(
+            column=3, row=name_count+8)
+        Label(main_window, text=(hire_details[name_count][3])).grid(
+            column=4, row=name_count+8)
+        name_count += 1
