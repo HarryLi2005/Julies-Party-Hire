000316b1f2aa3610d406e234dd05dc92cccc2211
Harry Li <reaperxxxiiii@gmail.com>
2022-06-26 17:26:32 +1200

    layout update

--- CSC2 Julies Party Hire.py
+++ CSC2 Julies Party Hire.py
@@ -83,11 +83,11 @@ def delete_row():
     total_entries = total_entries - 1
     delete_item.delete(0, 'end')
     # clear the last item displayed on the GUI
-    Label(main_window, text="       ").grid(column=0, row=name_count+7)
-    Label(main_window, text="       ").grid(column=1, row=name_count+7)
-    Label(main_window, text="       ").grid(column=2, row=name_count+7)
-    Label(main_window, text="       ").grid(column=3, row=name_count+7)
-    Label(main_window, text="       ").grid(column=4, row=name_count+7)
+    Label(main_window, text="                         ").grid(column=0, row=name_count+7)
+    Label(main_window, text="                         ").grid(column=1, row=name_count+7)
+    Label(main_window, text="                         ").grid(column=2, row=name_count+7)
+    Label(main_window, text="                         ").grid(column=3, row=name_count+7)
+    Label(main_window, text="                         ").grid(column=4, row=name_count+7)
     # print all the items in the list
     print_hire_details()
 
